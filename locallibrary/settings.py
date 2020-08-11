55343087a87a5a8e6f9455e005902e4ec6b47f36
leminhquang11 <quangleminh.leminh@gmail.com>
2020-08-11 09:44:24 +0700

    Django Tutorial Part 11: Deploying Django to production

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cbzzob-n2yk9y=-8f4qta493mg30m$nluih3y1t!(yso-)1vf$'
+# SECRET_KEY = 'cbzzob-n2yk9y=-8f4qta493mg30m$nluih3y1t!(yso-)1vf$'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cbzzob-n2yk9y=-8f4qta493mg30m$nluih3y1t!(yso-)1vf$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True)) != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['enigmatic-harbor-10316.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,3 +125,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
